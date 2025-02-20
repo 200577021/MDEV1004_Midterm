53560502e701cc4561f27a6c3925d9cf37ea2b9e
Aquilav_7 <200577021@student.georgianc.on.ca>
2025-02-20 18:33:29 -0500

    updating in server.js
    Collecting song data from songs.json file

--- server.js
+++ server.js
@@ -2,14 +2,23 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-
+const fs = require('fs')
+const path = require('path')
 const app = express();
 
 //middleware
 app.use(bodyParser.json());
 
+//songsData have data's from json file
+const songsData = JSON.parse(fs.readFileSync(path.join(__dirname, 'songs.json')));
+
+// defining API routes
+app.get('/api/songs', (req, res) => {
+    res.json(songsData);
+  });
+  
 //defining and starting server code
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
